ef955c5e5bc375d10406c7c259107c2e9e1882f5
sbaria <sbaria1402@gmail.com>
2020-12-29 20:34:37 +0000

    commit for branch1

---
+++ src/test/java/GitHubTestClasses/test1.java
@@ -0,0 +1,7 @@
+package GitHubTestClasses;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("my first class commit to github from branch1");
+    }
+}
