65c0a8836c9f2a7cc1b6f41ef48ec7e70d4eb016
sbaria <sbaria1402@gmail.com>
2020-12-30 20:18:54 +0000

    new class test1 commit to check fetch

--- src/test/java/GitHubTestClasses/test2.java
+++ src/test/java/GitHubTestClasses/test2.java
@@ -4,4 +4,7 @@ public class test2 {
     public static void main(String[] args) {
         System.out.println("last commit for fetch to local");
     }
+    public void fetchMaster(){
+        System.out.println("final pull and fetch to git local master");
+    }
 }
