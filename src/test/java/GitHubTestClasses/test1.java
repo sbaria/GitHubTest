dca95ccda35cc355f62f77a3082714c8cbc9cd37
sbaria <sbaria1402@gmail.com>
2020-12-30 20:02:46 +0000

    new class test1 commit to check fetch

--- src/test/java/GitHubTestClasses/test1.java
+++ src/test/java/GitHubTestClasses/test1.java
@@ -1,8 +1,27 @@
 package GitHubTestClasses;
 
-class test2 {
+public class test1 {
     public static void main(String[] args) {
-        System.out.println("checking final fetch and merge");
-    }
+        test1 ts=new test1();
+        ts.gitHubTest2();
+        ts.gitHubTest3();
+        ts.gitHubFetch2();
+        ts.branch4();
 
+        System.out.println("my first class commit to github from branch1");
+        System.out.println("checking fatch for origin/master");
+        System.out.println("checking pull requiest");
+    }
+    public void gitHubTest2(){
+        System.out.println("my commit for branch 2 which i will marge with master on gitHub and pull to local master");
+    }
+    public void gitHubTest3(){
+        System.out.println("i commit this change to check fetch command");
+    }
+    public void gitHubFetch2(){
+        System.out.println("i commit this change to check fetch command1");
+    }
+    public void branch4(){
+        System.out.println("final commit");
+    }
 }
---
+++ src/test/java/GitHubTestClasses/test2.java
@@ -0,0 +1,7 @@
+package GitHubTestClasses;
+
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("last commit for fetch to local");
+    }
+}
