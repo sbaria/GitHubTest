fe2983e1d3d5b668e963bc08fbe802b27c8505fe
sbaria <sbaria1402@gmail.com>
2020-12-31 13:45:55 +0000

    update by pull to branch

--- src/test/java/GitHubTestClasses/test1.java
+++ src/test/java/GitHubTestClasses/test1.java
@@ -9,8 +9,8 @@ public class test1 {
         ts.branch4();
 
         System.out.println("my first class commit to github from branch1");
-        System.out.println("checking fatch for origin/master");
-        System.out.println("checking pull requiest");
+        System.out.println("checking fetch for origin/master");
+        System.out.println("checking pull request");
     }
     public void gitHubTest2(){
         System.out.println("my commit for branch 2 which i will marge with master on gitHub and pull to local master");
